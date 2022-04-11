e27ff60dc5becfc887355a9da7dca6140794baa5
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-04-12 00:47:37 +0530

    Update autofilter.py

--- LuciferMoringstar_Robot/modules/autofilter.py
+++ LuciferMoringstar_Robot/modules/autofilter.py
@@ -43,8 +43,8 @@ async def group_filters(client, message):
         if not btn:
             return
 
-        if len(btn) > 10: 
-            btns = list(split_list(btn, 10)) 
+        if len(btn) > 05: 
+            btns = list(split_list(btn, 05)) 
             keyword = f"{message.chat.id}-{message.message_id}"
             BUTTONS[keyword] = {
                 "total" : len(btns),
@@ -137,8 +137,8 @@ async def pm_autofilter(client, message):
         if not btn:
             return
 
-        if len(btn) > 10: 
-            btns = list(split_list(btn, 10)) 
+        if len(btn) > 05: 
+            btns = list(split_list(btn, 05)) 
             keyword = f"{message.chat.id}-{message.message_id}"
             BUTTONS[keyword] = {
                 "total" : len(btns),
