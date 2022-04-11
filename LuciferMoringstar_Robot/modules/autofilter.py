356c68fef733326879890e4d0b2fd44053748f7f
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-04-12 00:02:57 +0530

    🔍

--- LuciferMoringstar_Robot/modules/autofilter.py
+++ LuciferMoringstar_Robot/modules/autofilter.py
@@ -21,9 +21,7 @@ async def group_filters(client, message):
                 file_id = file.file_id
                 file_name = file.file_name
                 file_size = get_size(file.file_size)
-                btn.append(
-                    [InlineKeyboardButton(text=f'🔮 {search}', url=f'{search})]
-                )
+                btn.append([InlineKeyboardButton(text=f"{search}", callback_data=f"{search})])
                 btn.append([
                             InlineKeyboardButton(text=f'🍭 {file_name}', url=f'samin#{file_id}'),
                             InlineKeyboardButton(text=f'🍬 {file_size}', url=f'samin#{file_id}')])
