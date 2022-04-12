5a62a12b2b8d8bd470546cdd769d35ab083c7991
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-13 00:51:08 +0530

    Create stats.py

---
+++ LuciferMoringstar_Robot/modules/stats.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters
+from LuciferMoringstar_Robot.database.users_chats_db import db
+from LuciferMoringstar_Robot.database.ia_filterdb import Media
+from LuciferMoringstar_Robot.database._utils import get_size
+
+@Client.on_message(filters.command('stats') & filters.incoming)
+async def get_ststs(bot, message):
+    rju = await message.reply('Fetching stats..')
+    total_users = await db.total_users_count()
+    totl_chats = await db.total_chat_count()
+    files = await Media.count_documents()
+    size = await db.get_db_size()
+    free = 536870912 - size
+    # size = get_size(size)
+    # free = get_size(free)
+    await rju.edit(script.STATUS_TXT.format(files, total_users, totl_chats, size, free))
