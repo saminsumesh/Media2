78ce6644ee8cc9ecef8f6c3c3f0ca179687d9cf0
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-13 15:22:39 +0530

    Update function.py

--- function.py
+++ function.py
@@ -11,34 +11,6 @@ async def encode(string):
     base64_string = (base64_bytes.decode("ascii")).strip("=")
     return base64_string
 
-async def decode(base64_string):
-    base64_string = base64_string.strip("=") # links generated before this commit will be having = sign, hence striping them to handle padding errors.
-    base64_bytes = (base64_string + "=" * (-len(base64_string) % 4)).encode("ascii")
-    string_bytes = base64.urlsafe_b64decode(base64_bytes) 
-    string = string_bytes.decode("ascii")
-    return string
-
-async def get_messages(client, message_ids):
-    messages = []
-    total_messages = 0
-    while total_messages != len(message_ids):
-        temb_ids = message_ids[total_messages:total_messages+200]
-        try:
-            msgs = await client.get_messages(
-                chat_id=client.db_channel.id,
-                message_ids=temb_ids
-            )
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            msgs = await client.get_messages(
-                chat_id=client.db_channel.id,
-                message_ids=temb_ids
-            )
-        except:
-            pass
-        total_messages += len(temb_ids)
-        messages.extend(msgs)
-    return messages
 
 async def get_message_id(client, message):
     if message.forward_from_chat:
@@ -64,4 +36,3 @@ async def get_message_id(client, message):
     else:
         return 0
 
-subscribed = filters.create(is_subscribed)
