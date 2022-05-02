84fe7554a6205bbbc8696b8657c160ac1cae780f
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-02 12:27:01 +0530

    Update commands.py

--- LuciferMoringstar_Robot/commands.py
+++ LuciferMoringstar_Robot/commands.py
@@ -9,7 +9,7 @@ from LuciferMoringstar_Robot.database.users_chats_db import db
 async def start_message(bot, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id)
-    if len(message.command) != 3:
+    if len(message.command) != 2:
         if message.from_user.id not in ADMINS: 
             buttons = [[
              InlineKeyboardButton("Add me to your group", url=f"http://t.me/{bot_info.BOT_USERNAME}?startgroup=true")
@@ -36,7 +36,7 @@ async def start_message(bot, message):
              ]]
         await message.reply_text(text=LuciferMoringstar.START_TXT.format(mention = message.from_user.mention, uptime = bot_time.uptime, version = __version__), reply_markup=InlineKeyboardMarkup(buttons))
         
-    elif len(message.command) ==3 and message.command[1] in ["subscribe"]:
+    elif len(message.command) ==2 and message.command[1] in ["subscribe"]:
         invite_link = await bot.create_chat_invite_link(int(FORCE_SUB))
         button=[[
          InlineKeyboardButton("ᴊᴏɪɴ ᴛᴏ ᴜsᴇ ᴍᴇ", url=invite_link.invite_link)
