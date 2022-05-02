1138779ad8cfadf4ae975d282b4f92fa7bf194ef
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-02 15:07:17 +0530

    Create cust_p_filters.py

---
+++ LuciferMoringstar_Robot/helpers/cust_p_filters.py
@@ -0,0 +1,9 @@
+async def admin_filter_f(filt, client, message):
+    return (
+        # t, lt, fl 2013
+        not message.edit_date
+        and await admin_check(message)
+    )
+
+
+admin_fliter = filters.create(func=admin_filter_f, name="AdminFilter")
