98123d75ddefa016181e5b56c305dbcb951d57be
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-21 04:08:17 +0530

    Update pin.py

--- LuciferMoringstar_Robot/modules/pin.py
+++ LuciferMoringstar_Robot/modules/pin.py
@@ -4,10 +4,11 @@ from pyrogram.types import (Message)
 
 @Client.on_message(filters.command("pin"))
 async def bot_pin(bot: Client, message: Message):
-  member = await bot.get_chat_member(message.char.id, message.from_user.id) 
+  member = await bot.get_chat_member(message.chat.id, message.from_user.id) 
   if member.can_pin_messages:
     if message.reply_to_message:
       await message.reply_to_message.pin()
+      await message.reply_text("Pinned Successfully")
     else:
       await message.reply("Reply to a message to pin")
   else:
