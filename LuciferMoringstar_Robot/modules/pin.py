a0387036d12f545ea3e5b7872d5c31707950a1c7
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-21 04:03:24 +0530

    Update pin.py

--- LuciferMoringstar_Robot/modules/pin.py
+++ LuciferMoringstar_Robot/modules/pin.py
@@ -4,21 +4,11 @@ from pyrogram.types import (Message)
 
 @Client.on_message(filters.command("pin"))
 async def bot_pin(bot: Client, message: Message):
-  chat_type = message.chat.type
-  reply = message.reply_to_message
-  if chat_type == "supergroup":
-    if reply:
-      try:
-        user = await bot.get_chat_member(message.chat.id, message.from_user.id)
-        if user.status != (("adminstrator") or ("owner")):
-          await message.reply_text("Poda Poi admin avh")
-        else:
-          await bot.pin_chat_message(message.chat.id, message_id)
-      except Exception as e:
-        print(e)
-    else:
-      await message.reply_text("Please reply to a message or any file so that i could pin it.")
+  member = await bot.get_chat_member(message.char.id, message.from_user.id) 
+  if member.can_pin_messages:
+    if message.reply_to_message:
+      await message.reply_to_message.pin()
+   else:
+      await message.reply("Reply to a message to pin")
   else:
-   await message.reply_text("OkDa")
-     
-  
+    await message.reply("You don't have permission to do this")
