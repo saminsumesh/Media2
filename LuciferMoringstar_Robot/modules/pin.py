92787d78a273f85f7637161ed3f704b12f4d48e5
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-21 03:53:31 +0530

    Update pin.py

--- LuciferMoringstar_Robot/modules/pin.py
+++ LuciferMoringstar_Robot/modules/pin.py
@@ -8,15 +8,17 @@ async def bot_pin(bot: Client, message: Message):
   reply = message.reply_to_message
   if chat_type == "supergroup":
     if reply:
-      user = await bot.get_chat_member(message.chat.id, message.from_user.id)
-      if user.status != (("adminstrator") or ("owner")):
-        await message.reply_text("Poda Poi admin avh")
-      else:
-        await bot.pin_chat_message(message.chat.id, message_id)
-        print("sussess")
+      try:
+        user = await bot.get_chat_member(message.chat.id, message.from_user.id)
+        if user.status != (("adminstrator") or ("owner")):
+          await message.reply_text("Poda Poi admin avh")
+      except Exception as e:
+        else:
+          await bot.pin_chat_message(message.chat.id, message_id)
+        print(e)
     else:
       await message.reply_text("Please reply to a message or any file so that i could pin it.")
   else:
-   return await message.reply_text("OkDa")
+   await message.reply_text("OkDa")
      
   
