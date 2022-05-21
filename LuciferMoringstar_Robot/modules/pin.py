2c4933052d787f569323b9b8b936c6d61e50bf09
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-21 13:01:52 +0530

    Update pin.py

--- LuciferMoringstar_Robot/modules/pin.py
+++ LuciferMoringstar_Robot/modules/pin.py
@@ -13,3 +13,16 @@ async def bot_pin(bot: Client, message: Message):
       await message.reply("Please reply to a message")
   else:
     await message.reply(f"Seems that you don't have enough rights to pin message in {message.chat.title}")
+
+
+@Client.on_message(filters.command("unpin"))
+async def bot_pin(bot: Client, message: Message):
+  user = await bot.get_chat_member(message.chat.id, message.from_user.id)
+  if user.can_pin_messages:
+    if message.reply_to_message:
+      await message.reply_to_message.unpin()
+      await message.reply_text(f"Successfully unpinned message")
+    else:
+      await message.reply("Please reply to a message")
+  else:
+    await message.reply(f"Seems that you don't have enough rights to unpin message in {message.chat.title}")
