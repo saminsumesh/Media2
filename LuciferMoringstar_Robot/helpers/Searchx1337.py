883015e3d34b06cb14fe64c5b0aa6850f391cccd
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-25 22:25:42 +0000

    Add files via upload

---
+++ LuciferMoringstar_Robot/helpers/Searchx1337.py
@@ -0,0 +1,9 @@
+import aiohttp
+from requests.utils import requote_uri
+
+API_1337X = "https://api.abirhasan.wtf/1337x?query={}&limit={}"
+
+async def Searchx1337(query: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(requote_uri(API_1337X.format(query, 50))) as res:
+            return (await res.json())["results"] if ((await res.json())).get("results", None) is not None) else []
