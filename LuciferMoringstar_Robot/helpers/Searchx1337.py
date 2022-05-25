81ce703b719f75d033d80b6addab4db501bc5fcb
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-25 22:30:28 +0000

    Update Searchx1337.py

--- LuciferMoringstar_Robot/helpers/Searchx1337.py
+++ LuciferMoringstar_Robot/helpers/Searchx1337.py
@@ -6,4 +6,4 @@ API_1337X = "https://api.abirhasan.wtf/1337x?query={}&limit={}"
 async def Searchx1337(query: str):
     async with aiohttp.ClientSession() as session:
         async with session.get(requote_uri(API_1337X.format(query, 50))) as res:
-            return (await res.json())["results"] if ((await res.json())).get("results", None) is not None) else []
+            return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
