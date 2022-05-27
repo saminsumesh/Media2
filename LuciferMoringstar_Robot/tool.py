5c0f5e4bbd147c41b5862563b4c6cb9858696266
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-27 06:02:17 +0530

    Update tool.py

--- LuciferMoringstar_Robot/tool.py
+++ LuciferMoringstar_Robot/tool.py
@@ -1,9 +1,31 @@
 import aiohttp
 from requests.utils import requote_uri
 
-API_1337X = "https://api.abirhasan.wtf/1337x?query={}&limit={}"
+API_1337x = "https://api.abirhasan.wtf/1337x?query={}&limit={}"
+API_YTS = "https://api.abirhasan.wtf/yts?query={}&limit={}"
+API_PIRATEBAY = "https://api.abirhasan.wtf/piratebay?query={}&limit={}"
+API_ANIME = "https://api.abirhasan.wtf/anime?query={}&limit={}"
 
-async def Searchx1337(query: str):
+
+async def Search1337x(query: str):
     async with aiohttp.ClientSession() as session:
-        async with session.get(requote_uri(API_1337X.format(query, 50))) as res:
+        async with session.get(requote_uri(API_1337x.format(query, 50))) as res:
+            return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
+
+
+async def SearchYTS(query: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(requote_uri(API_YTS.format(query, 50))) as res:
+            return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
+
+
+async def SearchPirateBay(query: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(requote_uri(API_PIRATEBAY.format(query, 50))) as res:
+            return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
+
+
+async def SearchAnime(query: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(requote_uri(API_ANIME.format(query, 50))) as res:
             return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
